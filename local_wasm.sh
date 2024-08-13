e028c324db82d28e1e02d7aead07201177f4405a
Frank Mayer <gh@frankmayer.dev>
2024-08-14 00:45:42 +0200

    build(wasm): fix typo

--- local_wasm.sh
+++ local_wasm.sh
@@ -3,6 +3,6 @@
 # cargo install wasm-bindgen-cli
 mkdir -p wasm
 cargo build --profile wasm-release --target wasm32-unknown-unknown --features "dev"
-wasm-bindgen --no-typescript --out-name leiden --out-dir wasm --target web target/wasm32-unknown-unknown/release/leiden.wasm
+wasm-bindgen --no-typescript --out-name leiden --out-dir wasm --target web target/wasm32-unknown-unknown/wasm-release/leiden.wasm
 wasm-opt -Os --output wasm/leiden_bg.wasm wasm/leiden_bg.wasm
 cp -r assets wasm/
