3e43a83e1602a11f4d11fb5d8c73ee6d4fd0831e
Frank Mayer <gh@frankmayer.dev>
2024-08-14 02:48:47 +0200

    feat: implemented wasm launcher

--- local_wasm.sh
+++ local_wasm.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 # cargo install wasm-bindgen-cli
-mkdir -p wasm
+cargo update -p wasm-bindgen
 cargo build --profile wasm-release --target wasm32-unknown-unknown --features "dev"
 wasm-bindgen --no-typescript --out-name leiden --out-dir wasm --target web target/wasm32-unknown-unknown/wasm-release/leiden.wasm
+echo 'export { __wbg_get_imports, __wbg_init_memory, __wbg_load, __wbg_finalize_init };' >> wasm/leiden.js
 wasm-opt -Os --output wasm/leiden_bg.wasm wasm/leiden_bg.wasm
 cp -r assets wasm/
